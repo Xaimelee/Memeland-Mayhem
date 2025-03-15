74b3452dbb909b1c1b9af321f7f291eb5614ddb2
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2025-03-15 21:55:44 +1300

    Stopping here for now. Using the firebase sdk for oauth is a big no go.

--- scenes/authentication/authentication.gd
+++ scenes/authentication/authentication.gd
@@ -1,5 +1,10 @@
 extends Node
 
+const HEADERS : PackedStringArray = [
+	"Content-Type: application/json",
+	"Accept: application/json",
+]
+
 @onready 
 var status_label := $Status
 
@@ -9,8 +14,45 @@ func _ready() -> void:
 	var provider: AuthProvider = Firebase.Auth.get_GoogleProvider()
 	var token = Firebase.Auth.get_token_from_url(provider)
 	if token != null:
-		Firebase.Auth.login_with_oauth(token, provider)
-		print(token)
+		#print(Firebase.Auth.auth.local_id)
+		print("here")
+		_send_request_for_access_token(token)
+		#var task = Firebase.Functions.execute("verifyIdToken", HTTPClient.METHOD_GET, {}, { "id_token": token })
+		#var result_dict = await task.function_executed
+		#print(result_dict)
+		#Firebase.Auth.login_with_oauth(token, provider)
+
+func _send_request_for_access_token(oauth_token: String) -> void:
+	var url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=AIzaSyCSNPAg2X0dIiUNJ8W3K5kAirGSu2RSwhw"
+	var request_body = {
+		"postBody": "id_token=" + oauth_token + "&providerId=google.com",
+		"requestUri": "http://localhost",
+		"returnSecureToken": true
+	}
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._on_request_completed)
+	var headers = ["Content-Type: application/json"]
+	var body = JSON.stringify(request_body)
+	var error = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
+	if error != OK:
+		print("Error sending request:", error)
+
+func _send_access_request(token: String) -> void:
+	print(token)
+	var url = "https://us-central1-memeland-mayhem-6e7a7.cloudfunctions.net/verifyIdToken"
+	var body = JSON.stringify({ "id_token": token })
+	var request = HTTPRequest.new()
+	add_child(request)
+	var error = request.request(url, HEADERS, HTTPClient.METHOD_POST, body)
+	if error != OK:
+		print("Error sending request:", error)
+
+func _on_request_completed(result, response_code, headers, body) -> void:
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	var response = json.get_data()
+	print(response)
 
 func _on_login_button_pressed() -> void:
 	var provider: AuthProvider = Firebase.Auth.get_GoogleProvider()
