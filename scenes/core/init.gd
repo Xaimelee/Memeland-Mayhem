06c3aec324b56f6b7e4a49f187c404e431be16c6
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2025-06-11 20:55:29 +1200

    Progress on the sync improvements

--- scenes/core/init.gd
+++ scenes/core/init.gd
@@ -9,11 +9,14 @@ func _ready() -> void:
 	# Server just needs to load into the game scene. 
 	if MultiplayerManager.is_local() or MultiplayerManager.is_server():
 		get_tree().change_scene_to_packed(MAIN_SCENE)
+		Globals.scene_changed.emit("Main")
 	# We force guest login if not on web build since connecting to wallet can...
 	#... only happen on the web build.
 	elif OS.get_name() != "Web":
 		get_tree().change_scene_to_packed(MAIN_MENU)
+		Globals.scene_changed.emit("Main Menu")
 	else:
 		# Login and player menu (inventory, stash, leveling).
 		get_tree().change_scene_to_packed(PLAYER_MENU)
+		Globals.scene_changed.emit("Player Menu")
 	
--- scenes/multiplayer/sync_instance.gd
+++ scenes/multiplayer/sync_instance.gd
@@ -5,12 +5,21 @@ class_name SyncInstance
 
 var network_id: int = 0
 var network_parent: SyncInstance = null
+var is_registered: bool = false
 # SyncInstance will always be a child component of the node we wanted synced
 @onready var root: Node = get_parent()
 
 func _ready() -> void:
+	if not is_registered and MultiplayerManager.is_server():
+		#incase onready hasnt fired
+		root = get_parent()
+		MultiplayerSync.register_sync_instance(self)
+	elif not is_registered and not MultiplayerManager.is_server():
+		root = get_parent()
+		queue_free()
+	print("ready")
 	#root.name = root.name + str(network_id)
-	test_stuff()
+	#test_stuff()
 
 func test_stuff() -> void:
 	print(root.get_path())
